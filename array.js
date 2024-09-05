e62fb75c0494da5c3c23205dfa555eb22ca84a6a
jnoyon <jihadur51@gmail.com>
2024-09-05 15:57:39 +0600

    add array.js

---
+++ array.js
@@ -0,0 +1,7 @@
+// array.lenth - count total element of an array
+let numbers = [45, 454, 787, 44, 78, 778, 685]
+console.log(numbers.length);
+
+// set or update (replace) values
+numbers[1] = 10;
+console.log(numbers);
